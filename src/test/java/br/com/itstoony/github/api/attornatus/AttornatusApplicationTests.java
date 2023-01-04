500a01c9330ec48b687a37f889f42b5f934373a9
itstoony <toonyrenner@gmail.com>
2023-01-04 16:06:21 -0300

    first commit

---
+++ src/test/java/br/com/itstoony/github/api/attornatus/AttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itstoony.github.api.attornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
