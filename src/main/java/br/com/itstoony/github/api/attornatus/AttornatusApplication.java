d76a83474e6946aff499816e6c5960987f09f85d
itstoony <toonyrenner@gmail.com>
2023-01-17 19:01:37 -0300

    Adding SpringDoc API configuration, documenting UserController

--- src/main/java/br/com/itstoony/github/api/attornatus/AttornatusApplication.java
+++ src/main/java/br/com/itstoony/github/api/attornatus/AttornatusApplication.java
@@ -2,8 +2,10 @@ package br.com.itstoony.github.api.attornatus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AttornatusApplication {
 
     public static void main(String[] args) {
